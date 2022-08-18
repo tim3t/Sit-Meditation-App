416ff8aba13ad21fc514f4990dc1a95267de2b66
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2022-08-17 21:02:05 -0400

    config heroku secret_key

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///sit_db')
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'izsekret'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'izsekret')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
